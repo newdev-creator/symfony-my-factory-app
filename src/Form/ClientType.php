74fe2fc8bb429781384407b2e88a564f8c31e316
Julien <newdevcreator@gmail.com>
2024-05-25 10:31:09 +0200

    FEAT: client controller

---
+++ src/Form/ClientType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ClientType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('factory')
+            ->add('address')
+            ->add('postalCode')
+            ->add('city')
+            ->add('email')
+            ->add('phone')
+            ->add('contactPrimary')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
