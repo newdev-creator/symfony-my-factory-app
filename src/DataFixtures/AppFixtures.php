abbd7e7edcbb75d9278234fb379aaea14980afc0
Julien <newdevcreator@gmail.com>
2024-05-25 18:30:54 +0200

    FEAT: fixtures user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,84 +2,100 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Client;
-use App\Entity\Order;
-use App\Entity\PlanAndSchema;
-use App\Entity\Product;
-use DateTimeImmutable;
 use Faker\Factory;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
+use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasherm
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
 
-        $clientsList = [];
-        $ordersList = [];
-        $productsList = [];
-        $plansAndSchemasList = [];
+        // EMPLOYEE
+        $basicUserList = [];
+        for ($i = 0; $i < 10; $i++) {
+            $birthday = \DateTimeImmutable::createFromMutable($faker->dateTime());
+            $basicUser = new User();
+            $manager->persist($basicUser);
 
-        for ($i = 0; $i < 20; $i++) {
-            $client = new Client();
-            $client
-                ->setFactory($faker->company)
-                ->setAddress($faker->address)
-                ->setPostalCode($faker->numberBetween(10000, 99999))
-                ->setCity($faker->city)
-                ->setEmail($faker->email)
-                ->setPhone($faker->phoneNumber)
-                ->setContactPrimary($faker->name)
-                ->setCreatedAt(new DateTimeImmutable('now'));
-            $clientsList[] = $client;
-            $manager->persist($client);
+            $basicUser
+                ->setEmail($faker->email())
+                ->setPostNumber($faker->randomDigit())
+                ->setStatus($faker->word())
+                ->setFirstName($faker->firstName())
+                ->setLastName($faker->lastName())
+                ->setBirthday($birthday)
+                ->setAddress($faker->address())
+                ->setPostalCode($faker->postcode())
+                ->setCity($faker->city())
+                ->setPhone($faker->phoneNumber())
+                ->setCreatedAt(new DateTimeImmutable('now'))
+                ->setRoles(['ROLE_EMPLOYEE']);
+
+            $hashedPassword = $this->passwordHasherm->hashPassword($basicUser, 'user');
+            $basicUser->setPassword($hashedPassword);
+            $basicUserList[] = $basicUser;
         }
 
-        for ($i = 0; $i < 20; $i++) {
-            $planAndSchema = new PlanAndSchema();
-            $planAndSchema
-                ->setTitle($faker->title())
-                ->setDescription($faker->sentence)
-                ->setImgUrl('img link')
-                ->setPdfUrl('pdf link')
-                ->setCreatedAt(new DateTimeImmutable('now'));
-            $plansAndSchemasList[] = $planAndSchema;
-            $manager->persist($planAndSchema);
+        // SECRETARY
+        for ($i = 0; $i < 1; $i++) {
+            $adminUser = new User();
+            $manager->persist($adminUser);
+
+            $adminUser
+                ->setEmail($faker->email())
+                ->setPostNumber($faker->randomDigit())
+                ->setStatus($faker->word())
+                ->setFirstName($faker->firstName())
+                ->setLastName($faker->lastName())
+                ->setBirthday($birthday)
+                ->setAddress($faker->address())
+                ->setPostalCode($faker->postcode())
+                ->setCity($faker->city())
+                ->setPhone($faker->phoneNumber())
+                ->setCreatedAt(new DateTimeImmutable('now'))
+                ->setRoles(['ROLE_SECRETARY']);
+
+            $hashedPassword = $this->passwordHasherm->hashPassword($adminUser, 'admin');
+            $adminUser->setPassword($hashedPassword);
+            $manager->persist($adminUser);
         }
 
-        for ($i = 0; $i < 20; $i++) {
-            $startDate = \DateTimeImmutable::createFromMutable($faker->dateTime());
-            $endDate = $faker->optional()->dateTime();
-            $endDateImmutable = $endDate ? \DateTimeImmutable::createFromMutable($endDate) : new DateTimeImmutable('now');
+        // DIRECTOR
+        for ($i = 0; $i < 1; $i++) {
+            $adminUser = new User();
+            $manager->persist($adminUser);
 
-            $product = new Product();
-            $product
-                ->setName($faker->word)
-                ->setDescription($faker->sentence)
-                ->setUnitPrice($faker->randomFloat(2, 1, 100))
-                ->setSupplier($faker->company)
-                ->setStartDate($startDate)
-                ->setEndDate($endDateImmutable)
-                ->setPlanAndSchema($plansAndSchemasList[array_rand($plansAndSchemasList)])
-                ->setCreatedAt(new DateTimeImmutable('now'));
-            // ->setStatus($faker->numberBetween(0, 10));
+            $adminUser
+                ->setEmail($faker->email())
+                ->setPostNumber($faker->randomDigit())
+                ->setStatus($faker->word())
+                ->setFirstName($faker->firstName())
+                ->setLastName($faker->lastName())
+                ->setBirthday($birthday)
+                ->setAddress($faker->address())
+                ->setPostalCode($faker->postcode())
+                ->setCity($faker->city())
+                ->setPhone($faker->phoneNumber())
+                ->setCreatedAt(new DateTimeImmutable('now'))
+                ->setRoles(['ROLE_DIRECTOR']);
 
-            $productsList[] = $product;
-            $manager->persist($product);
+            $hashedPassword = $this->passwordHasherm->hashPassword($adminUser, 'admin');
+            $adminUser->setPassword($hashedPassword);
+            $manager->persist($adminUser);
         }
 
-        for ($i = 0; $i < 20; $i++) {
-            $order = new Order();
-            $order
-                ->setStartDate(\DateTimeImmutable::createFromMutable($faker->dateTime()))
-                ->setClient($clientsList[array_rand($clientsList)])
-                ->setCreatedAt(new DateTimeImmutable('now'));
-            $ordersList[] = $order;
 
-            $manager->persist($order);
-        }
 
         $manager->flush();
     }
